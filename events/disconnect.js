04b623d510b5533782cf876c5418d5237eadeea1
Joele Buffolo <elia.stef006@gmail.com>
2022-09-10 15:30:30 +0200

    modified disconnect

--- events/disconnect.js
+++ events/disconnect.js
@@ -1,4 +1,4 @@
-const { VoiceState, Client } = require("discord.js");
+const { VoiceState, Client, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   name: "voiceStateUpdate",
@@ -12,8 +12,18 @@ module.exports = {
     if (oldstate.member.id != "928001727309946932") return;
     const deletequeue = (id) => oldstate.client.queue.delete(id);
     if (oldstate.channelId && !newstate.channel) {
-      if (oldstate.client.queue.get(newstate.guild.id)) {
-        newstate.client.queue.get(newstate.guild.id).player.stop();
+      const queue = oldstate.client.queue.get(newstate.guild.id);
+      if (queue) {
+        queue.message.channel.send({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(
+                "The bot was disconnect manually, Stopping the music!"
+              )
+              .setColor("DarkGreen"),
+          ],
+        });
+        queue.player.stop();
         return deletequeue(oldstate.guild.id);
       }
     }

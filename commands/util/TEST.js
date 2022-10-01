e47ba51c21fc18a8446c833b661a655601c63cbc
buffolollo <elia.stef006@gmail.com>
2022-10-01 23:06:48 +0200

    some tests

--- commands/util/TEST.js
+++ commands/util/TEST.js
@@ -1,7 +1,8 @@
-const { EmbedBuilder } = require("discord.js");
+const { Client, EmbedBuilder, Message } = require("discord.js");
 const fetch = require("isomorphic-unfetch");
 const spotify = require("spotify-url-info")(fetch);
 const searcher = require("youtube-sr").default;
+const ytsr = require("yt-search");
 let yt = require("ytdl-core");
 
 module.exports = {
@@ -9,12 +10,21 @@ module.exports = {
   aliases: [],
   d: "test command",
   staff: true,
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   * @returns
+   */
   async execute(client, message, args) {
-    const query = args[0];
+    const query = args.join(" ");
     if (!query) return error(message, "no query");
-
-    const data = await spotify.getData(query)
-    console.log(data.tracks.items[6].track.name, data.tracks.items[6].track.artists.map(a => a.name).join(" "))
-
+    const data = await searcher.search(query, { type: "video", limit: 1 });
+    const data2 = await ytsr(query);
+    // message.reply({
+    //   content: `${data.all[0].title}`
+    // })
+    // console.log(data.all[0].title)
   },
 };
